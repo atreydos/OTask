7ec9333a5d2807950495d31dfb8cdf73e38f9024
Vlad Derevyanko <3329251@gmail.com>
2020-09-08 09:25:21 +0300

    Added Basic custom  WebView integration

--- app/src/main/java/io/atreydos/otask/ui/widget/TrendDetailedWebView.kt
+++ app/src/main/java/io/atreydos/otask/ui/widget/TrendDetailedWebView.kt
@@ -2,12 +2,53 @@ package io.atreydos.otask.ui.widget
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+
 
 @SuppressLint("ViewConstructor")
 class TrendDetailedWebView(context: Context, url: String?) : WebView(context) {
 
-    init {
-        this.loadUrl(url)
+    private class InjectedAppJSInterface(private val context: Context) {
+
+        companion object {
+            const val INTERFACE_NAME = "InjectedApp"
+        }
+
+        @JavascriptInterface
+        fun ShowToast(message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
     }
+
+    private val mWebViewClient = object : WebViewClient() {
+
+        override fun shouldOverrideUrlLoading(view: WebView?, newUrl: String?): Boolean {
+            if (Uri.parse(newUrl).host == Uri.parse(url).host) {
+                // Навигация происходит в пределах домена - оверрайдить не нужно,
+                // позволить WebView загрузить страницу
+                return false
+            }
+            // Иначе, попросить OS Android отхэндлить URL
+            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(newUrl)))
+            return true
+        }
+
+    }
+
+    init {
+        @SuppressLint("SetJavaScriptEnabled")
+        settings.javaScriptEnabled = true
+        addJavascriptInterface(
+            InjectedAppJSInterface(context),
+            InjectedAppJSInterface.INTERFACE_NAME
+        )
+        webViewClient = mWebViewClient
+        loadUrl(url)
+    }
+
 }
